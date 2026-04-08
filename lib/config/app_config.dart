9af1a4b418bbfc75ed66c12a63f363882b9302d0
Mashiro <Mashiro070619@gmail.com>
2026-04-08 23:15:37 +0800

    Update app_config.dart
    Added update version URL and updated comments for clarity.

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -4,9 +4,22 @@ class AppConfig {
   static const schoolImportApiBaseUrl = String.fromEnvironment(
     'SCHOOL_IMPORT_API_BASE_URL',
     defaultValue: '',
-    // 你的api
+    // 你的ai api
   );
+
+  static const updateVersionUrl = String.fromEnvironment(
+    'CLASSMATE_UPDATE_VERSION_URL',
+    defaultValue: '',
+  );
+/* 你的更新接口，内容格式示例 {
+  "version": "v1.0.0",
+  "updateContent": "这里是更新公告"
+}
+*/
+  static const officialWebsiteUrl = 'https://mashiro.tech/classmate';
 
   static bool get hasSchoolImportApiBaseUrl =>
       schoolImportApiBaseUrl.trim().isNotEmpty;
+
+  static bool get hasUpdateVersionUrl => updateVersionUrl.trim().isNotEmpty;
 }
